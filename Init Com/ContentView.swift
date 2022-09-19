02948e3fe5620c1aac5d8397b1d497bcc17c9d61
RPower45 <ryanleighpower0@hotmil.com>
2022-09-19 15:09:15 +0100

    Initial Commit

---
+++ Init Com/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Init Com
+//
+//  Created by Ryan Leigh-Power on 19/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Init Com/Init_ComApp.swift
@@ -0,0 +1,17 @@
+//
+//  Init_ComApp.swift
+//  Init Com
+//
+//  Created by Ryan Leigh-Power on 19/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct Init_ComApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
